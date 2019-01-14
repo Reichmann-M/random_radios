96c5669f579d3bbc7f64878977f2e8e312ba1a60
Reichmann-M <46574997+Reichmann-M@users.noreply.github.com>
2019-01-14 12:23:33 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 let radios = [
   'http://vogtlandradio.mp3.green.ch/vogtland_128k_mp3', // vogtland
-  'http://streams.rsa-sachsen.de/100/mp3-192/streams.rsa-sachsen.de/play.m3u', // R.SA
-  'http://streams.radiopsr.de/101/mp3-192/streams.radiopsr.de/play.m3u', // RadioPSR
-  'http://avw.mdr.de/streams/284330-0_mp3_high.m3u', // MDR Sputnik
-  'http://stream.hitradio-rtl.de/HRRTL/mp3.m3u' // Hitradio
+  'http://streams.rsa-sachsen.de/100/mp3-192/streams.rsa-sachsen.de/play', // R.SA
+  'http://streams.radiopsr.de/101/mp3-192/streams.radiopsr.de/play', // RadioPSR
+  'http://avw.mdr.de/streams/284330-0_mp3_high', // MDR Sputnik
+  'http://stream.hitradio-rtl.de/HRRTL/mp3' // Hitradio
 ];
 
 function setup() {
