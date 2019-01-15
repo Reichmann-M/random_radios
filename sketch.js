3b95a7c0029bcfd883d1db24ac8c5b3f56e071da
Reichmann-M <46574997+Reichmann-M@users.noreply.github.com>
2019-01-15 09:55:31 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,10 +1,9 @@
-let radios = [
-  'http://vogtlandradio.mp3.green.ch/vogtland_128k_mp3', // vogtland
-  'http://streams.rsa-sachsen.de/100/mp3-192/streams.rsa-sachsen.de/play', // R.SA
-  'http://streams.radiopsr.de/101/mp3-192/streams.radiopsr.de/play', // RadioPSR
-  'http://avw.mdr.de/streams/284330-0_mp3_high', // MDR Sputnik
-  'http://stream.hitradio-rtl.de/HRRTL/mp3' // Hitradio
-];
+let radios;
+// sputnik http://avw.mdr.de/streams/284330-0_mp3_high
+
+function preload() {
+  radios = loadStrings('assets/radios.txt');
+}
 
 function setup() {
   noCanvas();
